364b9065ede8b9008e8f89d40a857d077c3db2ea
Puja0208 <pooja2081997@gmail.com>
2025-03-05 10:13:07 +0530

    feat: Add infinite scroll

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,45 @@
-import React, { useEffect, useState } from "react";
-import MemeCard from "./MemeCard";
+import { useEffect, useState } from "react";
+import { MemeCard } from "./MemeCard";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [memes, setMemes] = useState(null);
+  const [memes, setMemes] = useState([]);
+  const [showShimmer, setShowShimmer] = useState(false);
 
   useEffect(() => {
     fetchMemes();
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-  const fetchMemes = async () => {
-    const data = await fetch("https://meme-api.com/gimme/2");
-    const json = data.json();
+
+  const handleScroll = () => {
+    //scrollY - how much I have scrolled
+    // innerHeight - heigh of the window(visible setion)
+    // document.body.scrollHeight - total height of the web page
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+      fetchMemes();
+    }
   };
+
+  const fetchMemes = async () => {
+    setShowShimmer(true);
+    const data = await fetch("https://meme-api.com/gimme/20");
+    const json = await data.json();
+
+    setShowShimmer(false);
+    setMemes((memes) => [...memes, ...json.memes]);
+  };
+
   return (
     <div className="flex flex-wrap">
-      {!memes ? (
-        <Shimmer />
-      ) : (
-        memes.map((meme, i) => {
-          <MemeCard key={i} data={meme} />;
-        })
-      )}
+      {memes.map((meme, i) => (
+        <MemeCard key={i} data={meme} />
+      ))}
+
+      {showShimmer && <Shimmer />}
     </div>
   );
 };
-
 export default Body;
--- src/components/MemeCard.js
+++ src/components/MemeCard.js
@@ -1,11 +1,10 @@
-import React from "react";
-
-export default function MemeCard(data) {
+export const MemeCard = ({ data }) => {
   const { url, title, author } = data;
+
   return (
-    <div>
+    <div className="p-5 m-5 border border-black rounded-lg">
       <img className="w-64 h-64" alt="meme" src={url} />
-      <p>{title}</p>
+      <p>{author}</p>
     </div>
   );
-}
+};
